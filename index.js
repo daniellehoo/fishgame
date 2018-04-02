756380846450003b2ee2f8d86f4eec187124e477
Danielle Hoo <daniellehoo@Macintosh-106.local>
2018-04-01 20:46:07 -0400

    MVP plus additional design work

--- index.js
+++ index.js
@@ -1,37 +1,65 @@
-    /*eslint-env jquery*/
+/*eslint-env jquery*/
 
-// Step 1: Initialize game. Page loads and asks user their name. Name is stored in a variable
+// Step 1: Initialize game. Landing age loads and asks user their name. Name is stored in a variable
 // and referenced throughout the game (and used in the player's score)
 // Intro: addresses the player by their name and explains the rules in an alert. Users must click 'ok' to start the game.
-$(document).ready(function(){
-
-//   let name = prompt('Welcome to Fish Pond. What is your name?');
-
 // let intro = alert(`Hello ${name}! Here are the game rules: the object of the game is to catch as many goldfish as possible.
 // Fish are hiding in bubbles--be careful--there are pirnahas in the pond as well. Are you ready to play?`);
-
 // Step 2: Game play//gameboard and 'pawn' generation. Once a player clicks 'ok' the game start function initializes.
 // Bubbles are generated and begin moving within the pond div.
-
 // Step 3: Eventlisteners. Click events are added to each bubble. If a user clicks on a bubble, the bubble reveals
 // either a goldfish or a pirnaha.
+// Step 4: Scoring. If the user clicks on a goldfish, they get +1 point. If they click on a pirnaha, they get -1 point.
+// Step 5: Win scenario. Once a player reaches 10 points they win. Upon player win, they receive an alert indicating that they win.
+// Step 6: Reset button. Enable reset button that allows player to restart game.
+
+$(document).ready(() => {
+  if (window.location !== 'index.html') {
+    const playerName = [];
+
+    function landing() {
+      $('#nameForm').submit(function(event) {
+        // event.preventDefault();
+        const name = $('#nameForm :input').val();
+        playerName.push(name);
+        localStorage.setItem('playerName', name);
+        loadIndex();
+        function loadIndex() {
+          window.location.href = 'index.html';
+        }
+      })
+    }
+  }
+  landing();
+
+  const playerName = localStorage.getItem('playerName');
+
+  function pageSetup() {
+    const titleDiv = $('.title');
+    const title = $('<h1></h1>').text('Fish Pond');
+    const score = $('<h2></h2>')
+      .text(`${playerName}` + `'s Score: ` + 0)
+      .addClass('score');
+    let counter = $('<h3></h3>').addClass('timer');
+    titleDiv.append(title).append(score).append(counter);
+    setTime();
+}
+pageSetup();
 
 let fish = [];
 
 function createBubbles () {
-  for (let i = 0; i <= 10; i++) {
+  for (let i = 0; i < 30; i++) {
     let pond = $('.pond');
     let bubble = $('<div>').addClass('bubble');
       pond.append(bubble);
-    if (i % 2 === 0) {
-      bubble.addClass('goldfish');
       bubble.attr('id', 'fish' + i);
       fish.push(bubble);
-      bubble.on('click', reveal)
-    } else {
+      bubble.one('click', reveal)
+    if (i % 4 === 0) {
       bubble.addClass('pirnaha');
-      bubble.attr('id', 'fish' + i);
-      fish.push(bubble);
+    } else {
+      bubble.addClass('goldfish');
   }
 
   setInterval(function() {
@@ -43,25 +71,82 @@ function createBubbles () {
 
   }
 }
-
 createBubbles();
 
-function reveal(event) {
-  if ($(this).hasClass('goldfish')) {
+let counter = 0;
+let pond = $('.pond');
+let bubble = $('.bubble');
+let gameOver = $('.gameover');
+let restart = $('#restart');
+let playerScore = 0;
 
-    $(this).css('background-image', 'url(images/goldfish.png)');
-} else {
-    $(this).css('background-image', 'url(images/piranha.png)');
+function reveal(event) {
+  $(event.target).css('animation', 'none')
+  .css('border', 'none');
+  $(event.target).fadeOut().delay(7000);
+  $(event.target).fadeIn("fast");
+  if ($(event.target).hasClass('goldfish')) {
+    $(event.target).addClass('clicked');
+    playerScore += 1;
+    $('h2').text(`${playerName}` + `'s Score: ` + `${playerScore}`);
+  } else {
+    $(event.target).addClass('clicked');
+    playerScore -= 1;
+    $('h2').text(`${playerName}` + `'s Score: ` + `${playerScore}`);
   }
 }
 
-//Source: https://git.generalassemb.ly/wdi-nyc-rover/jquery-duck-hunt-lab
+function setTime() {
+  let counter = 20;
+  timer();
+  let time = setInterval(timer,1000);
 
-// //Step 4: Scoring. If the user clicks on a goldfish, they get +1 point. If they click on a pirnaha, they get -1 point.
-// let playerScore = $();
+  function timer() {
+  // let bubble = $('.bubble');
+    $('h3').html( `${timeString(counter)}` );
+    counter--;
+
+    if (counter === 0) {
+      $('h3').html( `${timeString(counter)}` );
+      clearInterval(time);
+      let gameText = $('<h4></h4>').html(`Game over, ${playerName}.
+      <br /> Your score: ${playerScore}`);
+      bubble.unbind('click');
+      gameOver.css('display', 'grid');
+      gameOver.prepend(gameText);
+   }
+  }
+
+  function timeString(seconds){
+    let minutesString = Math.floor(seconds / 60);
+    let secondsString = seconds % 60;
+    let resultString = 0;
+    if (secondsString < 10){
+      resultString = `Time Remaining: ${minutesString}:0${secondsString}`;
+    }
+    else {
+      resultString = `Time Remaining: ${minutesString}:${secondsString}`
+    }
+
+    return resultString;
+  }
+}
+
+ function restartGame () {
+   restart.click(function () {
+   gameOver.css('display', 'none');
+   // gameOver.remove(gameText);
+   location.reload();
+ })
+}
+ restartGame();
+
+//Source: https://git.generalassemb.ly/wdi-nyc-rover/jquery-duck-hunt-lab
+//Source: https://stackoverflow.com/questions/26253787/jquery-countdown-timer-for-games
 
 // //Step 5: Win scenario. Once a player reaches 10 points they win. Upon player win, they receive an alert indicating that they win.
 
 // //Step 6: Reset button. Enable reset button that allows player to restart game.
 
 });
+
